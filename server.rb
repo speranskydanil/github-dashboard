99c4f7c1acccdf5abb193b741626cb9d683c91d6
Speransky Danil <speranskydanil@gmail.com>
2014-03-24 13:57:11 +0700

    make requests local

--- server.rb
+++ server.rb
@@ -11,10 +11,3 @@ get '/' do
   haml :index
 end
 
-get '/api/' do
-  content_type :json
-  uri = "https://api.github.com/#{params[:query]}"
-  puts "GET #{uri}"
-  open(URI.encode(uri)).read
-end
-
